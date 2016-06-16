d76808343563c23c1e60159281eeacc21cdfa8de
Grant Ammons <gammons@gmail.com>
2016-06-16 14:50:18 -0400

    Remove search button

--- app/components/topbar/TodolistIconMenu.js
+++ app/components/topbar/TodolistIconMenu.js
@@ -5,7 +5,6 @@ import MenuItem from 'material-ui/MenuItem';
 import IconButton from 'material-ui/IconButton';
 
 import SortIcon from 'material-ui/svg-icons/content/sort';
-import SearchIcon from 'material-ui/svg-icons/action/search';
 import DoneAllIcon from 'material-ui/svg-icons/action/done-all';
 
 import FilterActions from "../../actions/FilterActions";
@@ -56,8 +55,6 @@ class TodolistIconMenu extends React.Component {
   render() {
     return(
       <div>
-        <IconButton iconStyle={{fill: "#ffffff"}} ><SearchIcon /></IconButton>
-
         <IconMenu
           iconButtonElement={<IconButton iconStyle={{fill: "#ffffff"}} ><SortIcon /></IconButton>}
           anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
