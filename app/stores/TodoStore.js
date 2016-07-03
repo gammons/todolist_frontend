2ab2186c34844b2687dc8c3a4b230853bb4e2e95
Grant Ammons <gammons@gmail.com>
2016-07-03 09:59:15 -0400

    Use local backend

--- app/stores/TodoStore.js
+++ app/stores/TodoStore.js
@@ -1,7 +1,7 @@
 import AppDispatcher from "../dispatchers/AppDispatcher";
 import { EventEmitter } from "events";
 import TodoRepo from "../TodoRepo";
-import Backend from "../backends/TestBackend";
+import Backend from "../backends/LocalBackend";
 import Constants from "../constants/Constants";
 
 const CHANGE_EVENT = "change_event";
