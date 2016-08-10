52f88b8b11d31d57fd1ac0c35157cd8f588dde77
Grant Ammons <gammons@gmail.com>
2016-08-10 07:41:06 -0400

    Add due date to todos, make it look good

--- app/components/todo.js
+++ app/components/todo.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import { ListGroup, ListGroupItem, Checkbox, DropdownButton, MenuItem } from 'react-bootstrap';
+import styles from '../styles.css'
 
 export default class Todo extends Component {
   render() {
+    const {todo} = this.props;
+
     return(
-      <ListGroupItem className="clearfix" key={this.props.todo.id}>
+      <ListGroupItem className="clearfix" key={todo.id}>
         <Checkbox inline className="clearfix" style={{marginTop: "13px"}}>
-          {this.props.todo.subject}
+          {todo.subject}
+          <br/>
+          <small>{this.formatDue(todo.due)}</small>
         </Checkbox>
         <span className="pull-right">
           <DropdownButton pullRight bsStyle={"link"} title="Actions" id={`button-${this.props.todo.id}`}>
@@ -20,5 +26,25 @@ export default class Todo extends Component {
         </span>
       </ListGroupItem>
     )
+  }
+
+  formatDue(due) {
+    if (due != null) {
+      due = moment(due).startOf("day");
+      let today = moment().startOf("day");
+      let tomorrow = moment().add(1,"day").startOf("day");
+
+      if (due.isBefore(today)) {
+        return (<div className={styles.red}>{due.format("ddd MMM D")}</div>);
+      }
+      if (today.isSame(due)) {
+        return (<div className={styles.blue}>Today</div>);
+      }
+
+      if (due.isSame(tomorrow)) {
+        return (<div className={styles.blue}>Tomorrow</div>);
+      }
+      return due.format("ddd MMM D");
+    }
   }
 }
