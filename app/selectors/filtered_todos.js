e278706b183464a7139edd7ec08209cbf86ab75f
Grant Ammons <gammons@gmail.com>
2016-08-17 20:42:40 -0400

    Add reselect library to contain the filtering logic

---
+++ app/selectors/filtered_todos.js
@@ -0,0 +1,21 @@
+import { createSelector  } from 'reselect';
+import Grouper from '../logic/grouper';
+import DateFilter from '../logic/date_filter';
+import ShowFilter from '../logic/show_filter';
+
+const todosSelector = state => state.todos.todos
+const filtersSelector = (state, props) => props.params
+
+const filterTodos = (todos, filter) => {
+  console.log("calling filter Todos")
+  const showFilter = new ShowFilter(todos)
+  const dateFilter = new DateFilter(showFilter.filterBy(filter.show));
+  const grouper = new Grouper(dateFilter.filterBy(filter.due));
+  return grouper.grouped(filter.group)
+}
+
+export default createSelector(
+  todosSelector,
+  filtersSelector,
+  filterTodos
+)
